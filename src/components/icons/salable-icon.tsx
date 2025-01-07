681f090c24e605a9cdb57591f641b0455e31bdfe
Perry George <pgeorge@adaptavist.com>
2025-01-07 12:50:23 +0000

    fix: footer design

---
+++ src/components/icons/salable-icon.tsx
@@ -0,0 +1,16 @@
+export const SalableIcon = ({height = 17, width = 17}:{height?:number,width?:number}) => (
+  <svg width={width} height={height} viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M9.21833 15.9284C12.6779 14.3152 14.1747 10.2029 12.5615 6.74326C12.5503 6.71923 12.5389 6.69529 12.5275 6.67145L0 12.5131C0.0109129 12.5372 0.0219633 12.5613 0.0331706 12.5853C1.6464 16.0449 5.75874 17.5417 9.21833 15.9284Z"
+      fill="url(#paint0_linear_718_18)"/>
+    <path
+      d="M7.67778 1.22024C4.21818 2.83348 2.72142 6.94581 4.33465 10.4054C4.34586 10.4294 4.35719 10.4534 4.36863 10.4772L16.8961 4.63556C16.8852 4.61147 16.8741 4.5874 16.8629 4.56337C15.2497 1.10377 11.1374 -0.392998 7.67778 1.22024Z"
+      fill="#48E4EF"/>
+    <defs>
+      <linearGradient id="paint0_linear_718_18" x1="6.63325" y1="12.4505" x2="12.8697" y2="6.21405" gradientUnits="userSpaceOnUse">
+        <stop stopColor="#4485FF"/>
+        <stop offset="1" stopColor="#4485FF" stopOpacity="0.6"/>
+      </linearGradient>
+    </defs>
+  </svg>
+)
